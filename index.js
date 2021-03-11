8009b12ca94f0eaf8292508926eb04da74b3af1c
Clinton Medbery <clintomed@gmail.com>
2021-03-11 11:54:40 -0500

    Basic Scraper Working

---
+++ index.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+let firebase = require("firebase/app");
+let _ = require('lodash');
+let zipcodes = require('zipcodes-nearby');
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+const zipData = require('./data/sc-zips.json');
+require('dotenv').config()
+
+let firebaseConfig = {
+    apiKey: process.env.GOOGLE_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const grab = async () => {
+    setTimeout(async () => { 
+        await page.goto('https://cvas.dhec.sc.gov/health/CovidVaccineScheduling/SelectLocation', { waitUntil: 'networkidle2' });
+        let cityData = zipData.map(x => x.fields)
+
+        let availableLocations = await page.evaluate(() => {
+
+            let data = []
+            let elements = document.getElementsByClassName('btn-outline-secondary')
+            for (var element of elements) {
+                let inner = element.innerHTML
+                inner = inner.replace('</b>','')
+                inner = inner.replace('<b>','')
+
+
+                let locArray = inner.split("<br>")
+                
+                if(locArray[3] !== 'Unavailable' || true){
+                    let name = locArray[0]
+                    let streetAddress = locArray[1]
+                    let cityStateZip = locArray[2]
+                
+                    let available = locArray[3] !== 'Unavailable'
+                   
+                    data.push({name, streetAddress, cityStateZip, available});
+                }
+                
+            }
+            return data
+        })
+
+        let availableLocMap = {}
+
+        availableLocations.forEach((loc) => {
+            let zipArray = loc.cityStateZip.split(" ")
+            let zip = zipArray.length > 1 ? zipArray[zipArray.length - 1] : ''
+            let info = _.find(cityData, (item) => item.zip == zip)
+            availableLocMap[zip] = {...loc, latitude: info && info.latitude, longitude: info && info.longitude}
+        })
+        let availableZips = Object.keys(availableLocMap)
+
+        const usersRef = await firebase.firestore().collection('users').onSnapshot((snapshot) => {
+            const userData = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }))
+            for(var user of userData){
+
+                let info = _.find(cityData, (item) => item.zip == user.zipCode)
+                if(!info){
+                    //Need to handle this
+                    return
+                }
+                console.log(user)
+                let nearby = zipcodes.near(user.zipCode.toString(), 30000, {datafile: 'data/sc-zips.csv', zipcode: "Zip", long: "Longitude", lat: "Latitude"}).then((nearby) => {
+                    let intersection = _.intersection(availableZips, nearby)
+                    console.log("intersection", intersection)
+                    if(intersection.length > 0){
+                        console.log("FOUND A MATCH")
+                    } else {
+                        console.log("NO MATCHES")
+                    }
+                })
+                let longitude = info.longitude
+                let latitude = info.latitude
+            }
+            
+            
+        })
+
+        await grab()
+    }, 1000)
+  }
+  await grab()
+  
+
+  // await browser.close();
+})();
